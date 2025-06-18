1c35f81ef872a92c22e285d162d29cf38e674a76
Nayan Das <nayan010das@gmail.com>
2025-06-18 21:08:32 +0530

    More Icons added

--- app/Icon.js
+++ app/Icon.js
@@ -1,10 +1,13 @@
 import { IoLogoJavascript } from "react-icons/io";
-import { FaCss3, FaHtml5, FaJava, FaPython, FaReact } from "react-icons/fa";
-import { RiNextjsFill } from "react-icons/ri";
+import { FaCss3, FaDatabase, FaHtml5, FaJava, FaPython, FaReact } from "react-icons/fa";
+import { RiNextjsFill, RiUserCommunityFill } from "react-icons/ri";
 import { MdQuiz } from "react-icons/md";
-import { PiMathOperationsFill } from "react-icons/pi";
-import { SiMongodb, SiMysql, SiTypescript } from "react-icons/si";
-import { TbBrandCpp } from "react-icons/tb";
+import { PiBrainDuotone, PiMathOperationsFill } from "react-icons/pi";
+import { SiMongodb, SiMysql, SiTypescript, SiWorldhealthorganization } from "react-icons/si";
+import { TbBrandCpp, TbMathSymbols } from "react-icons/tb";
+import { GiGiftOfKnowledge } from "react-icons/gi";
+import { IoHardwareChipSharp } from "react-icons/io5";
+
 export const ICONS = [
   { name: 'Quiz', icon: <MdQuiz  className="text-cyan-700 text-2xl" /> },
   { name: 'Java', icon: <FaJava className="text-orange-700 text-2xl" /> },
@@ -19,4 +22,11 @@ export const ICONS = [
   { name: 'SQL ', icon: <SiMysql className="text-blue-500 text-2xl" /> },
   { name: 'Mongodb  ', icon: <SiMongodb  className="text-green-600 text-2xl" /> },
   { name: 'Aptitude ', icon: <PiMathOperationsFill  className="text-gray-700 text-2xl" /> },
+  { name: 'Math ', icon: <TbMathSymbols  className="text-orange-500 text-2xl" /> },
+  { name: 'Brain', icon: <PiBrainDuotone  className="text-blue-500 text-2xl" /> },
+  { name: 'Verbal', icon: <RiUserCommunityFill className="text-blue-500 text-2xl" /> },
+  { name: 'Healthcare', icon: <SiWorldhealthorganization  className="text-blue-500 text-2xl" /> },
+  { name: 'Knowledge', icon: <GiGiftOfKnowledge className="text-orange-500 text-2xl" /> },
+  { name: 'Hardware', icon: <IoHardwareChipSharp className="text-gray-500 text-2xl" /> },
+  { name: 'Database', icon: <FaDatabase className="text-blue-500 text-2xl" /> },
 ];
