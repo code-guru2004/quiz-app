56d601a332922ece6440029e11860c182d78659b
Nayan Das <nayan010das@gmail.com>
2025-08-13 22:57:21 +0530

    UI update

--- app/page.js
+++ app/page.js
@@ -418,7 +418,7 @@ const QuizHomePage = () => {
                 Terms of Service
                 <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-white group-hover:w-full transition-all duration-300"></span>
               </a>
-              <a href="#" className="text-gray-300 hover:text-white transition duration-300 relative group">
+              <a href="/contact" className="text-gray-300 hover:text-white transition duration-300 relative group">
                 Support
                 <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-white group-hover:w-full transition-all duration-300"></span>
               </a>
