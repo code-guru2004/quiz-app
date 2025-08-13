519421ffda749df300bf49a2ef0ccbb5ed2e3d19
Nayan Das <nayan010das@gmail.com>
2025-08-13 22:53:56 +0530

    Ui update

--- app/terms-of-service/page.jsx
+++ app/terms-of-service/page.jsx
@@ -37,7 +37,7 @@ const TermsOfService = () => {
             <svg className="h-5 w-5 text-blue-500 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <span className="text-sm font-medium text-blue-700">For Test Portal Users</span>
+            <span className="text-sm font-medium text-blue-700">For Eduprobe Users</span>
           </div>
         </div>
 
